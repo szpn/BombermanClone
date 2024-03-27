2436f60920cd5386e019edabdf7eeca1604cd6ac
= <szopen1290@gmail.com>
2024-03-27 22:43:56 +0100

    added empty methods to game

--- model/game/Game.py
+++ model/game/Game.py
@@ -10,3 +10,27 @@ class Game:
     def addBombers(self, count):
         for i in range(count):
             self.bombers.append(Bomber([0, 0]))
+
+
+    def tick(self):
+        self.tickBombs()
+        self.pickupPowerUps()
+
+
+    def tickBombs(self):
+        pass
+
+    def pickupPowerUps(self):
+        pass
+
+    def moveBomber(self, bomber, direction):
+        if bomber not in self.bombers:
+            raise ValueError("The provided bomber is not in the game!")
+        bomber.move(direction)
+        #TODO: finish this
+
+    def placeBomb(self):
+        pass
+
+    def spawnPowerUp(self):
+        pass
