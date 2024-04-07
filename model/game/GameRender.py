4b905caa858a7e09c5461c161db077e544680ffd
szpn <szopen1290@gmail.com>
2024-04-07 14:15:34 +0200

    scaling images WIP

--- model/game/GameRender.py
+++ model/game/GameRender.py
@@ -4,6 +4,7 @@ class GameRender:
     def __init__(self, screen, game=None):
         self.screen = screen
         self.game = game
+        self.scale_factor = 2  # Define the scale factor
 
     def drawGame(self):
         self.cleanWindow()
@@ -19,15 +20,18 @@ class GameRender:
         for i in range(map.size):
             for j in range(map.size):
                 tile = map.map[i][j]
-                self.screen.blit(tile.sprite.image, tile.rect)
+                scaled_image = pygame.transform.scale(tile.sprite.image, (tile.sprite.image.get_width() * self.scale_factor, tile.sprite.image.get_height() * self.scale_factor))
+                self.screen.blit(scaled_image, (tile.rect.x * self.scale_factor, tile.rect.y * self.scale_factor))
 
     def drawBombers(self, bombers):
         for bomber in bombers:
-            self.screen.blit(bomber.sprite.image, bomber.rect)
+            scaled_image = pygame.transform.scale(bomber.sprite.image, (bomber.sprite.image.get_width() * self.scale_factor, bomber.sprite.image.get_height() * self.scale_factor))
+            self.screen.blit(scaled_image, (bomber.rect.x * self.scale_factor, bomber.rect.y * self.scale_factor))
 
     def drawPowerUps(self, powerups):
         for powerup in powerups:
-            self.screen.blit(powerup.sprite.image, powerup.rect)
+            scaled_image = pygame.transform.scale(powerup.sprite.image, (powerup.sprite.image.get_width() * self.scale_factor, powerup.sprite.image.get_height() * self.scale_factor))
+            self.screen.blit(scaled_image, (powerup.rect.x * self.scale_factor, powerup.rect.y * self.scale_factor))
 
     def setGameToRender(self, game):
         self.game = game
