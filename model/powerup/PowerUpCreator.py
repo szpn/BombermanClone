f7ece08028fa1043e99f5071a60c063a6b6604eb
= <szopen1290@gmail.com>
2024-04-08 21:07:50 +0200

    added bomb power power up

--- model/powerup/PowerUpCreator.py
+++ model/powerup/PowerUpCreator.py
@@ -1,11 +1,12 @@
 from random import choice
 
 from model.powerup.PowerUpBombCount import PowerUpBombCount
+from model.powerup.PowerUpBombPower import PowerUpBombPower
 from model.powerup.PowerUpHealth import PowerUpHealth
 
 
 class PowerUpCreator:
-    available_powerups = [PowerUpHealth, PowerUpBombCount]
+    available_powerups = [PowerUpHealth, PowerUpBombCount, PowerUpBombPower]
 
 
     @staticmethod
